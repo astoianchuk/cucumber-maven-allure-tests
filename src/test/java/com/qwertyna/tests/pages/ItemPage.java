3e374fa91bfbe07499d85be82a747a6d6d4213e5
astoianchuk <anastasia.stoianchuk@gmail.com>
2020-01-15 01:14:27 +0200

    Added TODO to improve screenshot view for @onTestCompleteWithScreenshot

--- src/test/java/com/qwertyna/tests/pages/ItemPage.java
+++ src/test/java/com/qwertyna/tests/pages/ItemPage.java
@@ -2,6 +2,7 @@ package com.qwertyna.tests.pages;
 
 import com.qwertyna.tests.DriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
@@ -13,18 +14,25 @@ public class ItemPage {
 
     @FindBy(how = How.CLASS_NAME, using = "details")
     private WebElement detailsPanel;
-    private By detailsPanelElement = By.tagName("tbody");
+    private By detailsPanelLocator = By.tagName("tbody");
 
     public ItemPage() {
         PageFactory.initElements(DriverManager.getInstance().getDriver(), this);
     }
 
     public boolean isContainCorrectFieldValue(String field, String value) {
-        List<WebElement> detailsList = detailsPanel.findElements(detailsPanelElement);
+        List<WebElement> detailsList = detailsPanel.findElements(detailsPanelLocator);
         for (int i = 1; i < detailsList.size(); i++) {
             if (detailsList.get(i).getText().contains(field + " " + value)) return true;
         }
         return false;
     }
+
+    //TODO fix and refactore next method for @onTestCompleteWithScreenshot
+    private void scrollToDetailsPanel(){
+//        WebElement element = DriverManager.getInstance().getDriver().findElement(detailsPanelLocator);
+//        ((JavascriptExecutor) DriverManager.getInstance().getDriver()).executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+
 
 }
